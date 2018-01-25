7231d6e953cc2b74fb2b3d6e20df084c0397df1d
Rizwan Atta <rizwanatta890@gmail.com>
2018-01-25 17:51:54 +0500

    PutExtra and some backbutton hacks

--- Intenter/app/src/main/java/com/infosys_sol/intenter/MainActivity.java
+++ Intenter/app/src/main/java/com/infosys_sol/intenter/MainActivity.java
@@ -5,27 +5,36 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.MediaController;
 
 public class MainActivity extends AppCompatActivity {
 
     FloatingActionButton fabTester;
+    EditText edtTxtName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         fabTester = findViewById(R.id.fab_tester);
+        edtTxtName = findViewById(R.id.edit_txt_name);
+
+
 
         fabTester.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String userEnteredText = edtTxtName.getText().toString();
 
-                Intent mintent = new Intent(MainActivity.this,SecondPageActivity.class);
-                startActivity(mintent);
-                finish();
-
+              Intent mintent = new Intent(MainActivity.this,SecondPageActivity.class);
+              mintent.putExtra("userName",userEnteredText);
+              startActivity(mintent);
+//              finish();
             }
         });
+
 
 
     }
